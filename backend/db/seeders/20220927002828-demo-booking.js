298f13832fab9616b8a964cdb2424202d143fe5c
mingliu <ming-liu@outlook.com>
2022-09-27 21:55:04 -0500

    seed file fix for heroku v1

--- backend/db/seeders/20220927002828-demo-booking.js
+++ backend/db/seeders/20220927002828-demo-booking.js
@@ -1,39 +1,39 @@
 'use strict';
 
-const { Booking } = require('../models');
+// const { Booking } = require('../models');
 
-const validBookings = [
-  {
-    spotId: 5,
-    userId: 1,
-    startDate: new Date('2022-10-19 15:00:00'),
-    endDate: new Date('2022-10-23 11:00:00'),
-  },
-  {
-    spotId: 4,
-    userId: 2,
-    startDate: '2022-10-05 15:00:00',
-    endDate: '2022-10-13 11:00:00',
-  },
-  {
-    spotId: 3,
-    userId: 3,
-    startDate: '2022-11-24 15:00:00',
-    endDate: '2022-11-27 11:00:00',
-  },
-  {
-    spotId: 2,
-    userId: 4,
-    startDate: '2022-12-24 15:00:00',
-    endDate: '2023-01-02 11:00:00',
-  },
-  {
-    spotId: 1,
-    userId: 5,
-    startDate: '2022-10-29 15:00:00',
-    endDate: '2022-11-02 11:00:00',
-  },
-]
+// const validBookings = [
+//   {
+//     spotId: 5,
+//     userId: 1,
+//     startDate: new Date('2022-10-19 15:00:00'),
+//     endDate: new Date('2022-10-23 11:00:00'),
+//   },
+//   {
+//     spotId: 4,
+//     userId: 2,
+//     startDate: '2022-10-05 15:00:00',
+//     endDate: '2022-10-13 11:00:00',
+//   },
+//   {
+//     spotId: 3,
+//     userId: 3,
+//     startDate: '2022-11-24 15:00:00',
+//     endDate: '2022-11-27 11:00:00',
+//   },
+//   {
+//     spotId: 2,
+//     userId: 4,
+//     startDate: '2022-12-24 15:00:00',
+//     endDate: '2023-01-02 11:00:00',
+//   },
+//   {
+//     spotId: 1,
+//     userId: 5,
+//     startDate: '2022-10-29 15:00:00',
+//     endDate: '2022-11-02 11:00:00',
+//   },
+// ]
 
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -45,9 +45,41 @@ module.exports = {
      * }], {});
     */
     //dynamic create with bulkCreate
-    await Booking.bulkCreate(validBookings, {
-      validate: true
-    });
+    // await Booking.bulkCreate(validBookings, {
+    //   validate: true
+    // });
+    await queryInterface.bulkInsert('Bookings', [
+      {
+        spotId: 5,
+        userId: 1,
+        startDate: new Date('2022-10-19 15:00:00'),
+        endDate: new Date('2022-10-23 11:00:00'),
+      },
+      {
+        spotId: 4,
+        userId: 2,
+        startDate: '2022-10-05 15:00:00',
+        endDate: '2022-10-13 11:00:00',
+      },
+      {
+        spotId: 3,
+        userId: 3,
+        startDate: '2022-11-24 15:00:00',
+        endDate: '2022-11-27 11:00:00',
+      },
+      {
+        spotId: 2,
+        userId: 4,
+        startDate: '2022-12-24 15:00:00',
+        endDate: '2023-01-02 11:00:00',
+      },
+      {
+        spotId: 1,
+        userId: 5,
+        startDate: '2022-10-29 15:00:00',
+        endDate: '2022-11-02 11:00:00',
+      },
+    ], {})
   },
 
   async down(queryInterface, Sequelize) {
