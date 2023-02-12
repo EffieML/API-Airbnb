23aa941808b6bb6ee705c831ae08ba485a206c98
mingliu <ming-liu@outlook.com>
2023-02-11 23:54:33 -0600

    fixed image bug

--- frontend/src/components/UserListingPage/index.js
+++ frontend/src/components/UserListingPage/index.js
@@ -54,7 +54,12 @@ function UserListingPage() {
                                 <div className='listed-spot'>
                                     <Link to={`/spots/${spot.id}`}>
                                         <div className='listed-spot-image'>
-                                            <img className='spot-image-size' src={spot.previewImage} onError={e => e.target.src = 'https://mingprojectawsbucket.s3.amazonaws.com/staybnbseed/imagesNotAvailable.png'} />
+                                            {spot.previewImage &&
+                                                <img className='spot-image-size' src={spot.previewImage} onError={e => e.target.src = 'https://mingprojectawsbucket.s3.amazonaws.com/staybnbseed/imagesNotAvailable.png'} />
+                                            }
+                                            {spot.SpotImages &&
+                                                <img className='spot-image-size' src={spot.SpotImages[0].url} onError={e => e.target.src = 'https://mingprojectawsbucket.s3.amazonaws.com/staybnbseed/imagesNotAvailable.png'} />
+                                            }
                                         </div>
                                     </Link>
                                     <div className='listed-spot-info'>
