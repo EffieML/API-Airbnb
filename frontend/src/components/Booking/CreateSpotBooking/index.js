f32b9bb1e4b154eddfdfff5faf55e614a61a1222
mingliu <ming-liu@outlook.com>
2023-02-17 00:07:02 -0600

    booking total price fixed

--- frontend/src/components/Booking/CreateSpotBooking/index.js
+++ frontend/src/components/Booking/CreateSpotBooking/index.js
@@ -26,11 +26,6 @@ function CreateSpotBooking({ spot }) {
         if (currUser) setShowLoginModal(false);
     }, [currUser]);
 
-    const totalPrice = (spotPrice) => {
-        console.log('total price------------', parseInt((spot.price + 10) * (calNights < 0 || isNaN(calNights) ? 0 : calNights) + 100))
-        console.log('total price2------------', parseInt((spot.price + serviceFee) * (calNights < 0 || isNaN(calNights) ? 0 : calNights) + cleanFee))
-        return parseInt((spot.price + serviceFee) * (calNights < 0 || isNaN(calNights) ? 0 : calNights) + cleanFee)
-    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -128,9 +123,6 @@ function CreateSpotBooking({ spot }) {
                 <div className='booking-form-bttm-r4'>
                     <div className='booking-form-bttm-left2'>Total before taxes</div>
                     <div className='booking-form-bttm-right2'>{`$${spot.price * calNights + serviceFee * calNights + cleanFee}`} </div>
-                    <div className='booking-form-bttm-right2'>{`$${(spot.price + serviceFee) * calNights + cleanFee}`} </div>
-                    <div className='booking-form-bttm-right2'>{`$${(Number(spot.price) + serviceFee) * calNights + cleanFee}`} </div>
-                    <div className='booking-form-bttm-right2'>${totalPrice(spot.price)} </div>
                 </div>
                 {showLoginModal && (
                     <Modal onClose={() => setShowLoginModal(false)}>
